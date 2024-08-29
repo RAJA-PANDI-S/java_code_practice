d54da53acb565b9f8cd0bc3339c101288b287e4e
RAJA-PANDI-S <rajapandirp2712@gmail.com>
2024-08-29 23:50:42 +0530

    CountNumOfOccurances and SumUntilZero class added

---
+++ src/CountNumOfOccurances.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CountNumOfOccurances {
+    public static void main(String[] args) {
+        int[] num = {2, 12, 5, 9, 0, 9, 5, 6, 4, 1, 9};
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number: ");
+        int value = sc.nextInt();
+        for(int i=0;i<num.length;i++){
+            if(num[i]==value){
+                count++;
+            }
+        }
+        System.out.println("Number "+value+" occurs "+count+" time(s) in a given array");
+    }
+}
+
+/*
+Output:
+=======
+Enter the Number:
+9
+Number 9 occurs 3 time(s) in a given array
+
+Process finished with exit code 0
+ */
---
+++ src/SumUntilZero.java
@@ -0,0 +1,61 @@
+// Sum of Even Positive and Odd Negative until user press 0
+
+import java.util.Scanner;
+
+public class SumUntilZero {
+    public static void main(String[] args) {
+        int temp1=0,temp2=0;
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Enter Even Positive or Odd Negative Number: ");
+            int value = sc.nextInt();
+            if (value > 0 && value%2==0) {
+                temp1 = temp1 + value;
+            }
+             else if (value < 0 && value%2!=0) {
+                temp2=temp2+value;
+            } else if (value == 0) {
+                System.out.println("Sum of Even Positive: "+temp1);
+                System.out.println("Sum of Odd Negative: "+temp2);
+                break;
+            }
+             else{
+                System.out.println("Incorrect Entry...");
+            }
+        }
+    }
+}
+
+/*
+Output:
+Enter Even Positive or Odd Negative Number:
+2
+Enter Even Positive or Odd Negative Number:
+-5
+Enter Even Positive or Odd Negative Number:
+7
+Incorrect Entry...
+Enter Even Positive or Odd Negative Number:
+6
+Enter Even Positive or Odd Negative Number:
+-1
+Enter Even Positive or Odd Negative Number:
+-4
+Incorrect Entry...
+Enter Even Positive or Odd Negative Number:
+0
+Sum of Even Positive: 8
+Sum of Odd Negative: -6
+
+Process finished with exit code 0
+
+ */
+
+
+
+
+
+
+
+
+
